4e63c8d011fd44c6b6afd15e20166db1c3b46628
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-04-21 20:03:20 +0545

    Set Password hide and visuable icon in login page

--- static/js/main.js
+++ static/js/main.js
@@ -305,20 +305,20 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  const eyeToggles = document.querySelectorAll('.eye-toggle');
+const eyeToggles = document.querySelectorAll('.eye-toggle');
 
-  eyeToggles.forEach(function(eyeToggle) {
-    eyeToggle.addEventListener('click', function() {
-      const passwordField = document.querySelector(this.getAttribute('toggle'));
-      if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-        this.classList.remove('fa-eye-slash');
-        this.classList.add('fa-eye');
-      } else {
-        passwordField.type = 'password';
-        this.classList.remove('fa-eye');
-        this.classList.add('fa-eye-slash');
-      }
-    });
-  });
+eyeToggles.forEach(function(eyeToggle) {
+eyeToggle.addEventListener('click', function() {
+    const passwordField = document.querySelector(this.getAttribute('toggle'));
+    if (passwordField.type === 'password') {
+    passwordField.type = 'text';
+    this.classList.remove('fa-eye-slash');
+    this.classList.add('fa-eye');
+    } else {
+    passwordField.type = 'password';
+    this.classList.remove('fa-eye');
+    this.classList.add('fa-eye-slash');
+    }
+});
+});
 });
